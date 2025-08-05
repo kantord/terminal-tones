338af6941f78ff9d9b773123d5e49da278d03565
Daniel Kantor <git@daniel-kantor.com>
2025-08-05 20:50:10 +0200

    cleanup

--- packages/theme-generator/src/palette.ts
+++ packages/theme-generator/src/palette.ts
@@ -12,23 +12,23 @@ type ReferencePaletteItem = [Okhsl, ReferencePaletteItemWeights] | [Okhsl]
 export type ReferencePalette = ReferencePaletteItem[]
 
 export const REFERENCE_PALETTE: ReferencePalette = [
-  [convertRgbToOkhsl([0, 0, 0]), { l: 2 }],   // 0 black
-  [convertRgbToOkhsl([128, 0, 0]), { h: 3, s: 2 }],   // 1 red (maroon)
-  [convertRgbToOkhsl([0, 128, 0]), { h: 3, s: 2 }],   // 2 green
-  [convertRgbToOkhsl([128, 128, 0]), { h: 3, s: 2 }],   // 3 yellow (olive)
-  [convertRgbToOkhsl([0, 0, 128])],   // 4 blue (navy)
-  [convertRgbToOkhsl([128, 0, 128])],   // 5 magenta (purple)
-  [convertRgbToOkhsl([0, 128, 128])],   // 6 cyan (teal)
-  [convertRgbToOkhsl([192, 192, 192])],   // 7 white (light-grey)
+  [convertRgbToOkhsl([0, 0, 0]), { l: 2 }],
+  [convertRgbToOkhsl([128, 0, 0]), { h: 3, s: 2 }],
+  [convertRgbToOkhsl([0, 128, 0]), { h: 3, s: 2 }],
+  [convertRgbToOkhsl([128, 128, 0]), { h: 3, s: 2 }],
+  [convertRgbToOkhsl([0, 0, 128])],
+  [convertRgbToOkhsl([128, 0, 128])],
+  [convertRgbToOkhsl([0, 128, 128])],
+  [convertRgbToOkhsl([192, 192, 192])],
 
-  [convertRgbToOkhsl([128, 128, 128])],   // 8 bright-black (dark-grey)
-  [convertRgbToOkhsl([255, 0, 0]), { h: 3, s: 2 }],   // 9 bright-red
-  [convertRgbToOkhsl([0, 255, 0]), { h: 3, s: 2 }],   // 10 bright-green
-  [convertRgbToOkhsl([255, 255, 0]), { h: 3, s: 2 }],   // 11 bright-yellow
-  [convertRgbToOkhsl([0, 0, 255])],   // 12 bright-blue
-  [convertRgbToOkhsl([255, 0, 255])],   // 13 bright-magenta
-  [convertRgbToOkhsl([0, 255, 255])],   // 14 bright-cyan
-  [convertRgbToOkhsl([255, 255, 255]), { l: 2 }],   // 15 bright-white
+  [convertRgbToOkhsl([128, 128, 128])],
+  [convertRgbToOkhsl([255, 0, 0]), { h: 3, s: 2 }],
+  [convertRgbToOkhsl([0, 255, 0]), { h: 3, s: 2 }],
+  [convertRgbToOkhsl([255, 255, 0]), { h: 3, s: 2 }],
+  [convertRgbToOkhsl([0, 0, 255])],
+  [convertRgbToOkhsl([255, 0, 255])],
+  [convertRgbToOkhsl([0, 255, 255])],
+  [convertRgbToOkhsl([255, 255, 255]), { l: 2 }],
 ];
 
 export function getPaletteScore(palette: Palette, referencePalette: ReferencePalette): number {
