263d58d15e3f24b3c12549f68b0b8a132979bdbd
Daniel Kantor <git@daniel-kantor.com>
2025-09-02 22:20:31 +0200

    add prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -8,11 +8,36 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    files: ["**/*.md"],
+    plugins: { markdown },
+    language: "markdown/gfm",
+    extends: ["markdown/recommended"],
+  },
+  {
+    files: ["**/*.css"],
+    plugins: { css },
+    language: "css/css",
+    extends: ["css/recommended"],
+  },
 ]);
--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,15 +1,14 @@
+import { Command } from "commander";
 
-import { Command } from 'commander'
+const program = new Command();
 
-const program = new Command()
+program
+  .name("terminal-tones")
+  .description("A command line tool for generating terminal color schemes");
 
+program
+  .command("from-image")
+  .description("Generate a color scheme from an image")
+  .argument("<path>", "path to image file");
 
-program.name('terminal-tones').description('A command line tool for generating terminal color schemes')
-
-
-program.command('from-image')
-  .description('Generate a color scheme from an image')
-  .argument('<path>', 'path to image file')
-
-
-program.parse()
+program.parse();
