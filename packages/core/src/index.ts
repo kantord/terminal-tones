8e6d2d90d8d1af694fdf7f791cfbb5c8d990582a
Daniel Kantor <git@daniel-kantor.com>
2025-09-13 22:57:44 +0200

    weight adjustments

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -37,22 +37,22 @@ export type DiffWeightOverrides = { wL?: number; wS?: number; wH?: number };
 export type RefEntry = [hex: string, weights?: DiffWeightOverrides];
 
 export const TERMINAL_16_REF: RefEntry[] = [
-  ["#000000", { wL: 1.5 }], // 0 black
-  ["#800000", { wH: 1.2 }], // 1 red
-  ["#008000", { wH: 1.2 }], // 2 green
+  ["#000000", { wL: 5, wH: 0, wS: 0 }], // 0 black
+  ["#800000", { wH: 5 }], // 1 red
+  ["#008000", { wH: 5 }], // 2 green
   ["#808000", { wL: 1.1 }], // 3 yellow
   ["#000080", { wH: 1.2 }], // 4 blue
   ["#800080", { wH: 1.2 }], // 5 magenta
   ["#008080", { wH: 1.2 }], // 6 cyan
-  ["#c0c0c0", { wL: 1.3 }], // 7 white (light gray)
-  ["#808080", { wL: 1.3 }], // 8 bright black (dark gray)
-  ["#ff0000"], // 9 bright red
-  ["#00ff00"], // 10 bright green
+  ["#c0c0c0", { wL: 5, wH: 0, wS: 0 }], // 7 white (light gray)
+  ["#808080", { wL: 5, wH: 0, wS: 0 }], // 8 bright black (dark gray)
+  ["#ff0000", { wH: 5 }], // 9 bright red
+  ["#00ff00", { wH: 5 }], // 10 bright green
   ["#ffff00", { wS: 1.1 }], // 11 bright yellow
   ["#0000ff"], // 12 bright blue
   ["#ff00ff"], // 13 bright magenta
   ["#00ffff"], // 14 bright cyan
-  ["#ffffff", { wL: 1.6 }], // 15 bright white
+  ["#ffffff", { wL: 5, wH: 0, wS: 0 }], // 15 bright white
 ];
 
 type OKHSL = {
